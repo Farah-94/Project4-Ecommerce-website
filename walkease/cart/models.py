f0ae52ba01e5ff15a75a22963a658d8c42242eb1
Farah-94 <farahshigri94@gmail.com>
2025-06-24 18:15:59 +0100

    Fix CartItem import issue

--- walkease/cart/models.py
+++ walkease/cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from walkease.store.models import Product
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    size = models.CharField(max_length=10)
+    def __str__(self):
+        return f"{self.product.name} - {self.size} - {self.quantity}"
+
