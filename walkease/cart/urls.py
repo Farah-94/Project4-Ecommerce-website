69b8d11f864a61e21d8e0d6d859e12213a12c3d8
Farah-94 <farahshigri94@gmail.com>
2025-06-26 08:51:36 +0100

    cart

--- walkease/cart/urls.py
+++ walkease/cart/urls.py
@@ -1,34 +1,18 @@
-# walkease/cart/urls.py
-
 from django.urls import path
 from . import views
-from allauth.account.views import SignupView  # you can keep using this
-from .views import CustomLoginView, logout_view, CustomSignupView, CustomLogoutView
+from .views import CustomSignupView, CustomLoginView, CustomLogoutView
+
 app_name = "cart"
 
 urlpatterns = [
     # Cart functionality
-    path("view/", views.cart_view, name="view_cart"),
-    path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
-    path("update/<int:item_id>/<str:action>/", views.update_cart, name="update_cart"),
-    path("remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("view/",      views.cart_view,        name="view_cart"),
+    path("add/<int:product_id>/",    views.add_to_cart,       name="add_to_cart"),
+    path("update/<int:item_id>/<str:action>/", views.update_cart,     name="update_cart"),
+    path("remove/<int:item_id>/",    views.remove_from_cart,  name="remove_from_cart"),
 
     # Allauth integration with your custom templates
-
-    
-   path(
-        "signup/",
-        CustomSignupView.as_view(),
-        name="account_signup",
-    ),
-    path(
-        "signin/",
-        CustomLoginView.as_view(),
-        name="account_login",
-    ),
-    path(
-        "signout/",
-        CustomLogoutView.as_view(),
-        name="account_logout",
-    ),
+    path("signup/", CustomSignupView.as_view(),  name="account_signup"),
+    path("signin/", CustomLoginView.as_view(),  name="account_login"),
+    path("signout/", CustomLogoutView.as_view(), name="account_logout"),
 ]
